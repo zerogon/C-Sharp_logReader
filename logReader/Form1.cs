9643640be1b4dc8cc12129e19d46e8df91588e49
Renovo <safoma@naver.com>
2019-10-10 21:10:55 +0900

    번호 입력값 받는 경우, 메소드 추출 미완성

--- logReader/Form1.cs
+++ logReader/Form1.cs
@@ -35,11 +35,38 @@ namespace logReader
             searchList.Items.Clear();
             ReadLog();
         }
+
+        private void ReadLogFile(DirectoryInfo di, int no)
+        {
+            foreach (var logFile in di.GetFiles())
+            {
+                string[] lines = File.ReadAllLines(logFile.FullName, Encoding.UTF8);
+                int searchCount = 0;
+                //해당로그에 검색키워드가 포함되어있는지 반복
+                foreach (string line in lines)
+                {
+                    if (line.Contains(keywordBox.Text))
+                    {
+                        searchCount++;
+                    }
+                }
+                ListViewItem lvi = new ListViewItem(no.ToString());
+                lvi.SubItems.Add("20191009");
+                lvi.SubItems.Add(jcodeBox.Text);
+                lvi.SubItems.Add("본점");
+                lvi.SubItems.Add("땡땡번호");
+                lvi.SubItems.Add(keywordBox.Text);
+                lvi.SubItems.Add(searchCount.ToString());
+                searchList.Items.Add(lvi);
+                no++;
+            }
+        }
+
         private void ReadLog()
         {
             //숫자아닌 빈값을 넣을경우 -> 전체검색
             if(String.IsNullOrEmpty(pnumber.Text)){
-                int no = 1; //검색건수
+                int no = 1; //검색건수 넘버링
                 List<string> arr = new List<string>();
                 string dirPath = @"C:\winformTest\" + jcodeBox.Text + @"\assemble\";
                 DirectoryInfo di = new DirectoryInfo(dirPath);
@@ -72,9 +99,43 @@ namespace logReader
                     }
                 }
             }
+            //땡땡번호가 입력된 경우
             else
             {
-                
+                int pnum;
+                //숫자가 아닌값이 들어온 경우 return
+                if (!int.TryParse(pnumber.Text, out pnum))
+                {
+                    MessageBox.Show("땡땡번호를 확인하세요.");
+                    return;
+                }
+                else
+                {
+                    string dirPath = @"C:\winformTest\" + jcodeBox.Text + @"\assemble\" + pnumber.Text;
+                    DirectoryInfo di = new DirectoryInfo(dirPath); //pFolder 안에있는 경로로 재설정
+                    foreach (var logFile in di.GetFiles())
+                    {
+                        string[] lines = File.ReadAllLines(logFile.FullName, Encoding.UTF8);
+                        int searchCount = 0;
+                        //해당로그에 검색키워드가 포함되어있는지 반복
+                        foreach (string line in lines)
+                        {
+                            if (line.Contains(keywordBox.Text))
+                            {
+                                searchCount++;
+                            }
+                        }
+                        ListViewItem lvi = new ListViewItem(no.ToString());
+                        lvi.SubItems.Add("20191009");
+                        lvi.SubItems.Add(jcodeBox.Text);
+                        lvi.SubItems.Add("본점");
+                        lvi.SubItems.Add("땡땡번호");
+                        lvi.SubItems.Add(keywordBox.Text);
+                        lvi.SubItems.Add(searchCount.ToString());
+                        searchList.Items.Add(lvi);
+                        no++;
+                    }
+                }
             }
            
         }
